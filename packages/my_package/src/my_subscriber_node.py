b7b4a2663f24d32b6224f646de4c5f3f546aabbb
serg <serg@oobuntoo.myguest.virtualbox.org>
2023-02-03 16:07:24 -0700

    boogie woogie

--- packages/my_package/src/my_subscriber_node.py
+++ packages/my_package/src/my_subscriber_node.py
@@ -1,27 +1,53 @@
 #!/usr/bin/env python3
-
+import numpy as np
 import os
 import rospy
-import numpy as np
-from duckietown.dtros import DTROS, NodeType
-from std_msgs.msg import String
-from sensor_msgs.msg import CameraInfo
+from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
+from duckietown_msgs.msg import Twist2DStamped, WheelEncoderStamped, WheelsCmdStamped
+from std_msgs.msg import Header, Float32
 
-
-class MySubscriberNode(DTROS):
+class OdometryNode(DTROS):
 
     def __init__(self, node_name):
-        # initialize the DTROS parent class
-        super(MySubscriberNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
-        # construct publisher
-        camTopic = '~/%s/camera_node/camera_info' % os.environ['VEHICLE_NAME']
-        self.sub = rospy.Subscriber(camTopic, CameraInfo, self.callback)
+        """Wheel Encoder Node
+        This implements basic functionality with the wheel encoders.
+        """
 
-    def callback(self, data):
-        rospy.loginfo("height of %s, width of %s frame id: %s", data.height, data.width, data.header.frame_id)
+        # Initialize the DTROS parent class
+        super(OdometryNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
+        self.veh_name = rospy.get_namespace().strip("/")
+
+        # Get static parameters
+        self._radius = rospy.get_param(f'/{self.veh_name}/kinematics_node/radius', 100)
+        encLeft = f'/{self.veh_name}/left_wheel_encoder_node/tick'
+        encRight= f'/{self.veh_name}/right_wheel_encoder_node/tick'
+        encCMD = f'/{self.veh_name}/wheels_driver_node/wheels_cmd_executed'
+        twist = f'/{self.veh_name}/kinematics_node/velocity'
+        
+        # Subscribing to the wheel encoders
+        #self.sub_encoder_ticks_left = rospy.Subscriber(encLeft, WheelEncoderStamped, lambda x: self.cb_encoder_data('left', x))
+        self.sub_encoder_ticks_right = rospy.Subscriber(encRight, WheelEncoderStamped, self.cb_encoder_data)
+        self.sub_executed_commands = rospy.Subscriber(encCMD, WheelsCmdStamped, self.cb_executed_commands)
+        #self.sub_executed_commands = rospy.Subscriber(twist, Twist2DStamped, self.cb_executed_commands)
+
+        # Publishers
+        #self.pub_integrated_distance_left = rospy.Publisher(...)
+        #self.pub_integrated_distance_right = rospy.Publisher(...)
+
+        self.log("Initialized")
+
+    def cb_encoder_data(self, wheel):
+        """ Update encoder distance information from ticks.
+        """
+        self.log(wheel)
+
+    def cb_executed_commands(self, msg):
+        """ Use the executed commands to determine the direction of travel of each wheel.
+        """
+        self.log(msg)
 
 if __name__ == '__main__':
-    # create the node
-    node = MySubscriberNode(node_name='my_subscriber_node')
-    # keep spinning
+    node = OdometryNode(node_name='my_odometry_node')
+    # Keep it spinning to keep the node alive
     rospy.spin()
+    rospy.loginfo("wheel_encoder_node is up and running...")
