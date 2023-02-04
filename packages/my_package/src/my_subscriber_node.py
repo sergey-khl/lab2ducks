1bfd53e3f3eb67d58ebfe304f53e6c038d0363b9
serg <serg@oobuntoo.myguest.virtualbox.org>
2023-02-03 17:47:36 -0700

    figuring out ticks

--- packages/my_package/src/my_subscriber_node.py
+++ packages/my_package/src/my_subscriber_node.py
@@ -21,18 +21,23 @@ class OdometryNode(DTROS):
         self._radius = rospy.get_param(f'/{self.veh_name}/kinematics_node/radius', 100)
         encLeft = f'/{self.veh_name}/left_wheel_encoder_node/tick'
         encRight= f'/{self.veh_name}/right_wheel_encoder_node/tick'
-        encCMD = f'/{self.veh_name}/wheels_driver_node/wheels_cmd_executed'
+        encCMD = f'/{self.veh_name}/wheels_driver_node/wheels_cmd'
         twist = f'/{self.veh_name}/kinematics_node/velocity'
 
         self.left_tick = 0
         self.left_dir = 0
+        self.left_last_data = 0
         
         self.right_tick = 0
         self.right_dir = 0
+        self.right_last_data = 0
+
+        self.dx_left = 0
+        self.dx_right = 0
         
         # Subscribing to the wheel encoders
         self.sub_encoder_ticks_left = rospy.Subscriber(encLeft, WheelEncoderStamped, lambda x: self.cb_encoder_data('left', x))
-        self.sub_encoder_ticks_right = rospy.Subscriber(encRight, WheelEncoderStamped, lambda x: self.cb_encoder_data('left', x))
+        self.sub_encoder_ticks_right = rospy.Subscriber(encRight, WheelEncoderStamped, lambda x: self.cb_encoder_data('right', x))
         self.sub_executed_commands = rospy.Subscriber(encCMD, WheelsCmdStamped, self.cb_executed_commands)
         #self.sub_executed_commands = rospy.Subscriber(twist, Twist2DStamped, self.cb_executed_commands)
 
@@ -45,11 +50,18 @@ class OdometryNode(DTROS):
     def cb_encoder_data(self, wheel, msg):
         """ Update encoder distance information from ticks.
         """
-        if (msg == 'left'):
-            self.left_tick += (wheel.data % wheel.resolution) * self.left_dir
-        elif (msg == 'right'):
-            self.right_tick += (wheel.data % wheel.resolution) * self.right_dir
-        self.log(self.right_tick)
+        if (wheel == 'left'):
+            self.left_tick += (msg.data-self.left_last_data)*self.left_dir
+            self.left_last_data = msg.data
+            self.dx_left = self.left_tick*self._radius*2*3.14/msg.resolution
+        elif (wheel == 'right'):
+            self.right_tick += (msg.data-self.right_last_data)*self.right_dir
+            self.right_last_data = msg.data
+            self.dx_right = self.right_tick*self._radius*2*3.14/msg.resolution
+        self.log(self._radius)
+        #self.log("right" + str(self.dx_right) + "left" + str(self.dx_left))
+        
+
 
     def cb_executed_commands(self, msg):
         """ Use the executed commands to determine the direction of travel of each wheel.
@@ -58,17 +70,18 @@ class OdometryNode(DTROS):
         if (msg.vel_left > 0):
             self.left_dir = 1
         elif (msg.vel_left < 0):
+            self.log("ISUDFIUSDFIUSDBFIUSDBF")
             self.left_dir = -1
-        else:
+        elif (msg.vel_left == 0):
             self.left_dir = 0
+
         if (msg.vel_right > 0):
             self.right_dir = 1
         elif (msg.vel_right < 0):
             self.right_dir = -1
-        else:
+        elif (msg.vel_right == 0):
             self.right_dir = 0
-        self.log(self.right_dir)
-        self.log(msg.vel_right)
+        #self.log(self.right_dir)
 
         
         
