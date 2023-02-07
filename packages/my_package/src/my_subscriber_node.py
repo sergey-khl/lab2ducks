e5b98994211614ea8bd413cb13a5a3b2bf6c465a
sergey <sergeykhlynovskiy@gmail.com>
2023-02-07 13:00:35 -0700

    forward back

--- packages/my_package/src/my_subscriber_node.py
+++ packages/my_package/src/my_subscriber_node.py
@@ -5,8 +5,6 @@ import rospy
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from duckietown_msgs.msg import Twist2DStamped, WheelEncoderStamped, WheelsCmdStamped
 from std_msgs.msg import Header, Float32
-import gym_duckietown
-from gym_duckietown.simulator import Simulator
 
 
 class OdometryNode(DTROS):
@@ -39,6 +37,13 @@ class OdometryNode(DTROS):
 
         self.dx_left = 0
         self.dx_right = 0
+
+        self.robot_frame = {
+            'x': 0,
+            'y': 0,
+            'theta': 0
+        }
+        self.stage = 0
         
         # Subscribing to the wheel encoders
         self.sub_encoder_ticks_left = rospy.Subscriber(encLeft, WheelEncoderStamped, lambda x: self.cb_encoder_data('left', x))
@@ -49,6 +54,7 @@ class OdometryNode(DTROS):
         # Publishers
         #self.pub_integrated_distance_left = rospy.Publisher(...)
         #self.pub_integrated_distance_right = rospy.Publisher(...)
+        self.pub_wheels_cmd = rospy.Publisher(encCMD, WheelsCmdStamped, queue_size=1)
 
         self.log("Initialized")
 
@@ -59,14 +65,33 @@ class OdometryNode(DTROS):
             if (self.left_last_data != 0):
                 self.left_tick += msg.data-self.left_last_data
             self.left_last_data = msg.data
-            self.dx_left = self.left_tick*self._radius*2*3.14/msg.resolution
+            self.dx_left = self.left_tick*self._radius*2*np.pi/msg.resolution
         elif (wheel == 'right'):
             if (self.right_last_data != 0):
                 self.right_tick += msg.data-self.right_last_data
             self.right_last_data = msg.data
-            self.dx_right = self.right_tick*self._radius*2*3.14/msg.resolution
+            self.dx_right = self.right_tick*self._radius*2*np.pi/msg.resolution
+
+        dA = (self.dx_left + self.dx_right)/(2*self._length)
+        self.robot_frame['x'] += dA*np.cos(self.robot_frame['theta'])
+        self.robot_frame['y'] += dA*np.sin(self.robot_frame['theta'])
+        self.robot_frame['theta'] += (self.dx_left - self.dx_right)/(2*self._length)
+
+        self.log(str(self.robot_frame['x']) + "   " + str(self.robot_frame['y']) + "   " + str(self.robot_frame['theta']))
+        msg_wheels_cmd = WheelsCmdStamped()
+        msg_wheels_cmd.header.stamp = msg.header.stamp
         if ((self.dx_left + self.dx_right)/2 >= 1.24 and (self.dx_left + self.dx_right)/2 <= 1.27):
-            self.log("SDOIFHSIODFIOSD")
+            msg_wheels_cmd.vel_right = -0.44
+            msg_wheels_cmd.vel_left = -0.44
+            self.stage = 1
+        elif (self.stage == 0):
+            msg_wheels_cmd.vel_right = 0
+            msg_wheels_cmd.vel_left = 0
+        else:
+            msg_wheels_cmd.vel_right = -0.44
+            msg_wheels_cmd.vel_left = -0.44
+        self.pub_wheels_cmd.publish(msg_wheels_cmd)
+
         
 
 
@@ -87,6 +112,18 @@ class OdometryNode(DTROS):
         elif (msg.vel_right == 0):
             self.right_dir = 0
 
+
+    # https://docs.duckietown.org/daffy/duckietown-robotics-development/out/new_duckiebot_functionality.html
+    def on_shutdown(self):
+        """Shutdown procedure.
+
+        Publishes a zero velocity command at shutdown."""
+        msg_wheels_cmd = WheelsCmdStamped()
+        msg_wheels_cmd.header.stamp = msg.header.stamp
+        msg_wheels_cmd.vel_right = 0
+        msg_wheels_cmd.vel_left = 0
+        self.pub_wheels_cmd.publish(msg_wheels_cmd)
+        super(OdometryNode, self).on_shutdown()
         
         
 
