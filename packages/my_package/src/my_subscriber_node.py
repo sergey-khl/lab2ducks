8dc1fa1288f487a428d405927796b527ccc77ddf
sergey <sergeykhlynovskiy@gmail.com>
2023-02-07 17:52:56 -0700

    synch signals maybe now can get robot pos

--- packages/my_package/src/my_subscriber_node.py
+++ packages/my_package/src/my_subscriber_node.py
@@ -5,6 +5,7 @@ import rospy
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from duckietown_msgs.msg import Twist2DStamped, WheelEncoderStamped, WheelsCmdStamped
 from std_msgs.msg import Header, Float32
+import message_filters
 
 
 class OdometryNode(DTROS):
@@ -29,11 +30,11 @@ class OdometryNode(DTROS):
 
         self.left_tick = 0
         self.left_dir = 0
-        self.left_last_data = 0
+        self.left_last_data = WheelsCmdStamped()
         
         self.right_tick = 0
         self.right_dir = 0
-        self.right_last_data = 0
+        self.right_last_data = WheelsCmdStamped()
 
         self.dx_left = 0
         self.dx_right = 0
@@ -46,10 +47,12 @@ class OdometryNode(DTROS):
         self.stage = 0
         
         # Subscribing to the wheel encoders
-        self.sub_encoder_ticks_left = rospy.Subscriber(encLeft, WheelEncoderStamped, lambda x: self.cb_encoder_data('left', x))
-        self.sub_encoder_ticks_right = rospy.Subscriber(encRight, WheelEncoderStamped, lambda x: self.cb_encoder_data('right', x))
-        self.sub_executed_commands = rospy.Subscriber(encCMD, WheelsCmdStamped, self.cb_executed_commands)
-        self.sub_kinematics = rospy.Subscriber(twist, Twist2DStamped, self.update)
+        self.sub_encoder_ticks_left = message_filters.Subscriber(encLeft, WheelEncoderStamped)
+        self.sub_encoder_ticks_right = message_filters.Subscriber(encRight, WheelEncoderStamped)
+        self.sub_encoder_ticks = message_filters.TimeSynchronizer([self.sub_encoder_ticks_left, self.sub_encoder_ticks_right], 10)
+        self.sub_encoder_ticks.registerCallaback(self.cb_encoder_data)
+        #self.sub_executed_commands = rospy.Subscriber(encCMD, WheelsCmdStamped, self.cb_executed_commands)
+        #self.sub_kinematics = rospy.Subscriber(twist, Twist2DStamped, self.update)
 
         # Publishers
         #self.pub_integrated_distance_left = rospy.Publisher(...)
@@ -58,49 +61,51 @@ class OdometryNode(DTROS):
         
         self.log("Initialized")
 
-    def cb_encoder_data(self, wheel, msg):
+    def cb_encoder_data(self, msgLeft, msgRight):
         """ Update encoder distance information from ticks.
         """
-        if (wheel == 'left'):
-            if (self.left_last_data != 0):
-                self.left_tick += msg.data-self.left_last_data
-            self.left_last_data = msg.data
-            self.dx_left = self.left_tick*self._radius*2*np.pi/msg.resolution
-        elif (wheel == 'right'):
-            if (self.right_last_data != 0):
-                self.right_tick += msg.data-self.right_last_data
-            self.right_last_data = msg.data
-            self.dx_right = self.right_tick*self._radius*2*np.pi/msg.resolution
+        if (self.left_last_data != 0):
+            self.left_tick += msgLeft.data-self.left_last_data
+        self.left_last_data.data = msgLeft.data
+        self.dx_left = self.left_tick*self._radius*2*np.pi/msgLeft.resolution
+
+        if (self.right_last_data != 0):
+            self.right_tick += msgRight.data-self.right_last_data
+        self.right_last_data.data = msgRight.data
+        self.right_last_data.header = msgRight.header
+        self.dx_right = self.right_tick*self._radius*2*np.pi/msgLeft.resolution
 
         dA = (self.dx_left + self.dx_right)/(2*self._length)
-        self.robot_frame['x'] = dA*np.cos(self.robot_frame['theta'])
-        self.robot_frame['y'] = dA*np.sin(self.robot_frame['theta'])
-        self.robot_frame['theta'] = (self.dx_left - self.dx_right)/(2*self._length)
-
-        
-
+        self.robot_frame['x'] += dA*np.cos(self.robot_frame['theta'])
+        self.robot_frame['y'] += dA*np.sin(self.robot_frame['theta'])
+        self.robot_frame['theta'] += (self.dx_right - self.dx_left)/(2*self._length)
+        self.dx_right = 0 
+        self.dx_left = 0
+        self.right_tick = 0
+        self.left_tick = 0
+        self.log(str(self.robot_frame['x']) + "   " + str(self.robot_frame['y']) + "   " + str(self.robot_frame['theta']))
+        msg_wheels_cmd = WheelsCmdStamped()
+        #msg_wheels_cmd.header.stamp = msg.header.stamp
+        if (self.stage == 0):
+            if ((self.dx_left + self.dx_right)/2 >= 1.24 and (self.dx_left + self.dx_right)/2 <= 1.27):
+                self.stage = 1
+            msg_wheels_cmd.vel_right = 0.44
+            msg_wheels_cmd.vel_left = 0.44
+        elif (self.stage == 1):
+            if ((self.dx_left + self.dx_right)/2 >= -0.01 and (self.dx_left + self.dx_right)/2 <= 0.03 and self.stage == 1):
+                self.stage = 2
+            msg_wheels_cmd.vel_right = -0.44
+            msg_wheels_cmd.vel_left = -0.44
+        elif (self.stage == 2):
+            msg_wheels_cmd.vel_right = 0
+            msg_wheels_cmd.vel_left = 0
+        self.pub_wheels_cmd.publish(msg_wheels_cmd)
 
 
     def update(self):
         rate = rospy.Rate(10) # 10Hz
         while not rospy.is_shutdown():
-            self.log(str(self.robot_frame['x']) + "   " + str(self.robot_frame['y']) + "   " + str(self.robot_frame['theta']))
-            msg_wheels_cmd = WheelsCmdStamped()
-            #msg_wheels_cmd.header.stamp = msg.header.stamp
-            if (self.stage == 0):
-                if ((self.dx_left + self.dx_right)/2 >= 1.24 and (self.dx_left + self.dx_right)/2 <= 1.27):
-                    self.stage = 1
-                msg_wheels_cmd.vel_right = 0.44
-                msg_wheels_cmd.vel_left = 0.44
-            elif (self.stage == 1):
-                if ((self.dx_left + self.dx_right)/2 >= -0.01 and (self.dx_left + self.dx_right)/2 <= 0.03 and self.stage == 1):
-                    self.stage = 2
-                msg_wheels_cmd.vel_right = -0.44
-                msg_wheels_cmd.vel_left = -0.44
-            elif (self.stage == 2):
-                msg_wheels_cmd.vel_right = 0
-                msg_wheels_cmd.vel_left = 0
-            self.pub_wheels_cmd.publish(msg_wheels_cmd)
+            
             rate.sleep()
         
         
@@ -139,7 +144,7 @@ if __name__ == '__main__':
     node = OdometryNode(node_name='my_odometry_node')
     # Keep it spinning to keep the node alive
     try:
-        node.update()
+        #node.update()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
