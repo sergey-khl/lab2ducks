972bb9bb82bc60288a982de9f96d12f09a79f12f
sergey <sergeykhlynovskiy@gmail.com>
2023-02-07 21:57:36 -0700

    robot goes straight!

--- packages/my_package/src/my_subscriber_node.py
+++ packages/my_package/src/my_subscriber_node.py
@@ -29,12 +29,10 @@ class OdometryNode(DTROS):
         twist = f'/{self.veh_name}/kinematics_node/velocity'
 
         self.left_tick = 0
-        self.left_dir = 0
-        self.left_last_data = WheelsCmdStamped()
+        self.left_last_data = 0
         
         self.right_tick = 0
-        self.right_dir = 0
-        self.right_last_data = WheelsCmdStamped()
+        self.right_last_data = 0
 
         self.dx_left = 0
         self.dx_right = 0
@@ -45,86 +43,77 @@ class OdometryNode(DTROS):
             'theta': 0
         }
         self.stage = 0
+
+        # Publishers
+        self.pub_wheels_cmd = rospy.Publisher(encCMD, WheelsCmdStamped, queue_size=1)
         
         # Subscribing to the wheel encoders
         self.sub_encoder_ticks_left = message_filters.Subscriber(encLeft, WheelEncoderStamped)
         self.sub_encoder_ticks_right = message_filters.Subscriber(encRight, WheelEncoderStamped)
-        self.sub_encoder_ticks = message_filters.TimeSynchronizer([self.sub_encoder_ticks_left, self.sub_encoder_ticks_right], 10)
-        self.sub_encoder_ticks.registerCallaback(self.cb_encoder_data)
+        self.sub_encoder_ticks = message_filters.ApproximateTimeSynchronizer([self.sub_encoder_ticks_left, self.sub_encoder_ticks_right], 10, 0.1, allow_headerless=True)
+        self.sub_encoder_ticks.registerCallback(self.cb_encoder_data)
         #self.sub_executed_commands = rospy.Subscriber(encCMD, WheelsCmdStamped, self.cb_executed_commands)
         #self.sub_kinematics = rospy.Subscriber(twist, Twist2DStamped, self.update)
 
-        # Publishers
-        #self.pub_integrated_distance_left = rospy.Publisher(...)
-        #self.pub_integrated_distance_right = rospy.Publisher(...)
-        self.pub_wheels_cmd = rospy.Publisher(encCMD, WheelsCmdStamped, queue_size=1)
-        
+    
         self.log("Initialized")
 
     def cb_encoder_data(self, msgLeft, msgRight):
         """ Update encoder distance information from ticks.
         """
         if (self.left_last_data != 0):
-            self.left_tick += msgLeft.data-self.left_last_data
-        self.left_last_data.data = msgLeft.data
+            self.left_tick = msgLeft.data-self.left_last_data
+        self.left_last_data = msgLeft.data
         self.dx_left = self.left_tick*self._radius*2*np.pi/msgLeft.resolution
 
         if (self.right_last_data != 0):
-            self.right_tick += msgRight.data-self.right_last_data
-        self.right_last_data.data = msgRight.data
-        self.right_last_data.header = msgRight.header
-        self.dx_right = self.right_tick*self._radius*2*np.pi/msgLeft.resolution
+            self.right_tick = msgRight.data-self.right_last_data
+        self.right_last_data = msgRight.data
+        self.dx_right = self.right_tick*self._radius*2*np.pi/msgRight.resolution
 
-        dA = (self.dx_left + self.dx_right)/(2*self._length)
+        dA = (self.dx_left + self.dx_right)/2
+        
         self.robot_frame['x'] += dA*np.cos(self.robot_frame['theta'])
         self.robot_frame['y'] += dA*np.sin(self.robot_frame['theta'])
         self.robot_frame['theta'] += (self.dx_right - self.dx_left)/(2*self._length)
-        self.dx_right = 0 
-        self.dx_left = 0
-        self.right_tick = 0
-        self.left_tick = 0
+
         self.log(str(self.robot_frame['x']) + "   " + str(self.robot_frame['y']) + "   " + str(self.robot_frame['theta']))
         msg_wheels_cmd = WheelsCmdStamped()
-        #msg_wheels_cmd.header.stamp = msg.header.stamp
+        msg_wheels_cmd.header.stamp = msgLeft.header.stamp
         if (self.stage == 0):
-            if ((self.dx_left + self.dx_right)/2 >= 1.24 and (self.dx_left + self.dx_right)/2 <= 1.27):
+            if (self.robot_frame['x'] >= 1.23 and self.robot_frame['x'] <= 1.27):
                 self.stage = 1
-            msg_wheels_cmd.vel_right = 0.44
-            msg_wheels_cmd.vel_left = 0.44
+            if (self.robot_frame['y'] > 0.01):
+                msg_wheels_cmd.vel_right = 0.35
+                msg_wheels_cmd.vel_left = 0.4
+            elif (self.robot_frame['y'] < -0.01):
+                msg_wheels_cmd.vel_right = 0.4
+                msg_wheels_cmd.vel_left = 0.35
+            else:
+                msg_wheels_cmd.vel_right = 0.4
+                msg_wheels_cmd.vel_left = 0.4
+
         elif (self.stage == 1):
-            if ((self.dx_left + self.dx_right)/2 >= -0.01 and (self.dx_left + self.dx_right)/2 <= 0.03 and self.stage == 1):
+            if (self.robot_frame['x'] >= -0.02 and self.robot_frame['x'] <= 0.01):
                 self.stage = 2
-            msg_wheels_cmd.vel_right = -0.44
-            msg_wheels_cmd.vel_left = -0.44
+
+            if (self.robot_frame['y'] > 0.01):
+                msg_wheels_cmd.vel_right = -0.35
+                msg_wheels_cmd.vel_left = -0.4
+            elif (self.robot_frame['y'] < -0.01):
+                msg_wheels_cmd.vel_right = -0.4
+                msg_wheels_cmd.vel_left = -0.35
+            else:
+                msg_wheels_cmd.vel_right = -0.4
+                msg_wheels_cmd.vel_left = -0.4
         elif (self.stage == 2):
             msg_wheels_cmd.vel_right = 0
             msg_wheels_cmd.vel_left = 0
         self.pub_wheels_cmd.publish(msg_wheels_cmd)
 
-
-    def update(self):
-        rate = rospy.Rate(10) # 10Hz
-        while not rospy.is_shutdown():
-            
-            rate.sleep()
-        
-        
-    def cb_executed_commands(self, msg):
-        """ Use the executed commands to determine the direction of travel of each wheel.
-        """
-        if (msg.vel_left > 0):
-            self.left_dir = 1
-        elif (msg.vel_left < 0):
-            self.left_dir = -1
-        elif (msg.vel_left == 0):
-            self.left_dir = 0
-
-        if (msg.vel_right > 0):
-            self.right_dir = 1
-        elif (msg.vel_right < 0):
-            self.right_dir = -1
-        elif (msg.vel_right == 0):
-            self.right_dir = 0
+    # move to position relative to robot
+    def move_forward(self, x, y):
+pass
 
 
     # https://docs.duckietown.org/daffy/duckietown-robotics-development/out/new_duckiebot_functionality.html
@@ -132,10 +121,11 @@ class OdometryNode(DTROS):
         """Shutdown procedure.
 
         Publishes a zero velocity command at shutdown."""
-        msg_wheels_cmd = WheelsCmdStamped()
-        msg_wheels_cmd.vel_right = 0
-        msg_wheels_cmd.vel_left = 0
-        self.pub_wheels_cmd.publish(msg_wheels_cmd)
+        for i in range(3):
+            msg_wheels_cmd = WheelsCmdStamped()
+            msg_wheels_cmd.vel_right = 0
+            msg_wheels_cmd.vel_left = 0
+            self.pub_wheels_cmd.publish(msg_wheels_cmd)
         super(OdometryNode, self).on_shutdown()
         
         
@@ -144,7 +134,6 @@ if __name__ == '__main__':
     node = OdometryNode(node_name='my_odometry_node')
     # Keep it spinning to keep the node alive
     try:
-        #node.update()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
